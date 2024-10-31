59bede4b922d9d569baaf1877188f714dc23eca2
Chiming Ni <chiming2@illinois.edu>
2024-10-31 10:29:26 -0700

    More interfaces

--- mp_ooo/hdl/common/interfaces/backend_itf.sv
+++ mp_ooo/hdl/common/interfaces/backend_itf.sv
@@ -24,18 +24,42 @@ import cpu_params::*;
 
     logic                   valid;
     logic                   ready;
-    logic   [31:0]          data;
+    logic   [ROB_IDX-1:0]   rob_id;
+    logic   [PRF_IDX-1:0]   rd_phy;
+    logic   [ARF_IDX-1:0]   rd_arch;
+    logic   [PRF_IDX-1:0]   rs1_phy;
+    logic                   rs1_valid;
+    logic   [PRF_IDX-1:0]   rs2_phy;
+    logic                   rs2_valid;
+    logic                   imm;
+    logic                   opcode; // TODO: Determine opcodes
 
     modport id (
         output              valid,
         input               ready,
-        output              data
+        output              rob_id,
+        output              rd_phy,
+        output              rd_arch,
+        output              rs1_phy,
+        output              rs1_valid,
+        output              rs2_phy,
+        output              rs2_valid,
+        output              imm,
+        output              opcode
     );
 
     modport int_rs (
         input               valid,
-        output              ready,
-        input               data
+        input               ready,
+        input               rob_id,
+        input               rd_phy,
+        input               rd_arch,
+        input               rs1_phy,
+        input               rs1_valid,
+        input               rs2_phy,
+        input               rs2_valid,
+        input               imm,
+        input               opcode
     );
 
 endinterface
@@ -70,18 +94,72 @@ endinterface
 interface id_rat_itf();
 import cpu_params::*;
 
-    logic                   valid;
-    logic                   ready;
-    logic   [31:0]          data;
+    logic   [ARF_IDX-1:0]   read_arch[2];
+    logic   [PRF_IDX-1:0]   read_phy[2];
+    logic   [PRF_IDX-1:0]   read_valid[2];
+    logic                   write_en;
+    logic   [ARF_IDX-1:0]   write_arch;
+    logic   [PRF_IDX-1:0]   write_phy;
 
     modport id (
-        output              valid,
-        input               ready
+        output              read_arch,
+        input               read_phy,
+        input               read_valid,
+        output              write_en,
+        output              write_arch,
+        output              write_phy
     );
 
     modport rat (
+        input               read_arch,
+        output              read_phy,
+        output              read_valid,
+        input               write_en,
+        input               write_arch,
+        input               write_phy
+    );
+
+endinterface
+
+interface id_fl_itf();
+import cpu_params::*;
+
+    logic                   valid;
+    logic                   ready;
+    logic   [PRF_IDX-1:0]   free_idx;
+
+    modport id (
+        output              valid,
+        input               ready,
+        input               free_idx
+    );
+
+    modport fl (
         input               valid,
-        output              ready
+        output              ready,
+        output              free_idx
+    );
+
+endinterface
+
+
+interface rob_rrf_itf();
+import cpu_params::*;
+
+    logic                   valid;
+    logic   [PRF_IDX-1:0]   rd_phy;
+    logic   [ARF_IDX-1:0]   rd_arch;
+
+    modport rob (
+        output              valid,
+        output              rd_phy,
+        output              rd_arch
+    );
+
+    modport rrf (
+        input               valid,
+        input               rd_phy,
+        input               rd_arch
     );
 
 endinterface
@@ -116,6 +194,12 @@ import cpu_params::*;
 
     modport rob (
         input               rob_id,
+        input               valid
+    );
+
+    modport rat (
+        input               rd_phy,
+        input               rd_arch,
         input               valid
     );
 
