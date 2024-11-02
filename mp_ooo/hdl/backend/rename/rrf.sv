d4e6885728f553ee5542de6e0ecc29acfa97f2b6
siyingyu <siyingy3@illinois.edu>
2024-11-02 07:05:39 -0500

    Add rrf

--- mp_ooo/hdl/backend/rename/rrf.sv
+++ mp_ooo/hdl/backend/rename/rrf.sv
@@ -8,5 +8,24 @@ import cpu_params::*;
     rrf_fl_itf.rrf              to_fl
 );
 
+    logic     [PRF_IDX-1:0]     mem [ARF_DEPTH];
+    always_ff @( posedge clk ) begin
+        if( rst ) begin 
+            for( int i = 0; i < ARF_DEPTH; i++ ) begin
+                mem[i] <= {1'b0, ARF_IDX'(i)};  // sync with rat
+            end
+        end else begin
+            if( from_rob.valid ) 
+                mem[from_rob.rd_arch] <= from_rob.rd_phy;
+        end
+    end
+    always_comb begin
+        to_fl.valid = '0;
+        to_fl.stale_idx = 'x;
+        if( from_rob.valid ) begin
+            to_fl.valid = '1;
+            to_fl.stale_idx = mem[from_rob.rd_arch];
+        end
+    end
 
 endmodule
