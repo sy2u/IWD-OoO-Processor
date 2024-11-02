9ed8dd74e41ee32c8dbc8fa6319b3f170f74724d
kongninglai <kongning.lai@gmail.com>
2024-11-02 18:54:47 +0800

    [CP2-RS&EX] refactor tb

--- mp_ooo/hvl/vcs/execute_tb.sv
+++ mp_ooo/hvl/vcs/execute_tb.sv
@@ -33,45 +33,45 @@ module execute_tb;
         .cdb                    (cdb_itfs)
     );
     
-    initial begin
-        $fsdbDumpfile("dump.fsdb");
-        $fsdbDumpvars(0, "+all");
+    task do_reset() 
         rst = 1'b1;
-        // set cdb and rs_prf_itfs to 0
-        cdb_itfs[1].valid = 1'b0;
-        cdb_itfs[1].rob_id = 'x;
-        cdb_itfs[1].rd_phy = 'x;
-        cdb_itfs[1].rd_arch = 'x;
-        cdb_itfs[1].rd_value = 'x;
-
-        rs_prf_itfs[1].rs1_phy   = '0;
-        rs_prf_itfs[1].rs2_phy   = '0;
-
         repeat (2) @(posedge clk);
         rst <= 1'b0;
+        // set cdb and rs_prf_itfs to 0
+        cdb_itfs[1].valid <= 1'b0;
+        cdb_itfs[1].rob_id <= 'x;
+        cdb_itfs[1].rd_phy <= 'x;
+        cdb_itfs[1].rd_arch <= 'x;
+        cdb_itfs[1].rd_value <= 'x;
 
+        rs_prf_itfs[1].rs1_phy   <= '0;
+        rs_prf_itfs[1].rs2_phy   <= '0;
+
+        id_int_rs_itf_i.uop.pc <= 'x;
+        id_int_rs_itf_i.uop.fu_opcode <= 'x;
+        id_int_rs_itf_i.uop.op1_sel <= 'x;
+        id_int_rs_itf_i.uop.op2_sel <= 'x;
+        id_int_rs_itf_i.uop.rd_phy <= 'x;
+        id_int_rs_itf_i.uop.rs1_phy <= 'x;
+        id_int_rs_itf_i.uop.rs2_phy <= 'x;
+        id_int_rs_itf_i.uop.rs1_valid <= 1'b0;
+        id_int_rs_itf_i.uop.rs2_valid <= 1'b0;
+        id_int_rs_itf_i.uop.imm <= 'x;
+        id_int_rs_itf_i.uop.rob_id <= 'x;
+        id_int_rs_itf_i.uop.rd_arch <= 'x;
+        id_int_rs_itf_i.valid <= 1'b0;
+
+        repeat (2) @(posedge clk);
+    endtask
+
+    task test_single_instruction() 
+        // single add
         // x1 (x1) = x2 + 1
-        // cycle 0:
-        // pc <= 0
-        // fu_opcode <= ALU_ADD
-        // op1_sel   <= OP1_RS1
-        // op2_sel   <= OP2_IMM
-        // rd_phy    <= 6'd1
-        // rs1_phy   <= 6'd2
-        // rs2_phy   <= 6'd0
-        // rs1_valid <= 1'b1
-        // rs2_valid <= 1'b0
-        // imm <= 20'd1
-        // rob_id <= '0
-        // rd_arch <= 5'd1
-        // from_id.valid <= 1'b1
-        id_int_rs_itf_i.uop.pc <= '0;
         id_int_rs_itf_i.uop.fu_opcode <= ALU_ADD;
         id_int_rs_itf_i.uop.op1_sel <= OP1_RS1;
         id_int_rs_itf_i.uop.op2_sel <= OP2_IMM;
         id_int_rs_itf_i.uop.rd_phy <= 6'd1;
         id_int_rs_itf_i.uop.rs1_phy <= 6'd2;
-        id_int_rs_itf_i.uop.rs2_phy <= 6'd0;
         id_int_rs_itf_i.uop.rs1_valid <= 1'b1;
         id_int_rs_itf_i.uop.rs2_valid <= 1'b0;
         id_int_rs_itf_i.uop.imm <= 32'd1;
@@ -83,6 +83,47 @@ module execute_tb;
         // from_id.valid <= 1'b0
         repeat (1) @(posedge clk);
         id_int_rs_itf_i.valid <= 1'b0;
+    endtask
+    initial begin
+        $fsdbDumpfile("dump.fsdb");
+        $fsdbDumpvars(0, "+all");
+        do_reset();
+
+        test_single_instruction();
+        // rst = 1'b1;
+        // // set cdb and rs_prf_itfs to 0
+        // cdb_itfs[1].valid = 1'b0;
+        // cdb_itfs[1].rob_id = 'x;
+        // cdb_itfs[1].rd_phy = 'x;
+        // cdb_itfs[1].rd_arch = 'x;
+        // cdb_itfs[1].rd_value = 'x;
+
+        // rs_prf_itfs[1].rs1_phy   = '0;
+        // rs_prf_itfs[1].rs2_phy   = '0;
+
+        // repeat (2) @(posedge clk);
+        // rst <= 1'b0;
+
+        // // single add
+        // // x1 (x1) = x2 + 1
+        // id_int_rs_itf_i.uop.pc <= '0;
+        // id_int_rs_itf_i.uop.fu_opcode <= ALU_ADD;
+        // id_int_rs_itf_i.uop.op1_sel <= OP1_RS1;
+        // id_int_rs_itf_i.uop.op2_sel <= OP2_IMM;
+        // id_int_rs_itf_i.uop.rd_phy <= 6'd1;
+        // id_int_rs_itf_i.uop.rs1_phy <= 6'd2;
+        // id_int_rs_itf_i.uop.rs2_phy <= 6'd0;
+        // id_int_rs_itf_i.uop.rs1_valid <= 1'b1;
+        // id_int_rs_itf_i.uop.rs2_valid <= 1'b0;
+        // id_int_rs_itf_i.uop.imm <= 32'd1;
+        // id_int_rs_itf_i.uop.rob_id <= '0;
+        // id_int_rs_itf_i.uop.rd_arch <= 5'd1;
+        // id_int_rs_itf_i.valid <= 1'b1;
+
+        // // cycle 1
+        // // from_id.valid <= 1'b0
+        // repeat (1) @(posedge clk);
+        // id_int_rs_itf_i.valid <= 1'b0;
 	repeat (10) @(posedge clk);
 	$finish;
         
