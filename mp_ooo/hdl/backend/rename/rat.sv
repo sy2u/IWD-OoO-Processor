e9158662ef599c921fbbe1413f5cc8a27fbb0de0
siyingy3 <siyingy3@linux-ssh-06.ews.illinois.edu>
2024-11-02 01:59:18 -0500

    init RAT

--- mp_ooo/hdl/backend/rename/rat.sv
+++ mp_ooo/hdl/backend/rename/rat.sv
@@ -8,6 +8,34 @@ import cpu_params::*;
     cdb_itf.rat                 cdb[CDB_WIDTH]
 );
 
+    logic   [PRF_IDX:0]         mem [ARF_DEPTH];    // msb for valid
+
+    always_ff @( posedge clk ) begin
+        if( rst ) begin
+            for( int i = 0; i < ARF_DEPTH; i++ ) begin
+                mem[i][PRF_IDX] <= '1;
+                mem[i][PRF_IDX-1:0] <= ARF_DEPTH'(unsigned(i));
+            end
+        end else begin
+            if( from_id.write_en ) begin
+                mem[i][PRF_IDX] <= '0;
+                mem[from_id.write_arch][PRF_IDX-1:0] <= from_id.write_phy;
+            end
+            for( int i = 0; i < CDB_WIDTH; i++ ) begin
+                if( cdb[i].valid && (mem[cdb[i].rd_arch][PRF_IDX-1:0] == cdb[i].rd_phy) )
+                    mem[cdb[i].rd_arch][PRF_IDX] <= '1;
+            end
+        end
+    end
+
+    always_comb begin
+        from_id.read_phy = mem[from_id.read_arch];
+        // transparent RAT
+        for( int i = 0; i < CDB_WIDTH; i++ ) begin
+            if( cdb[i].valid && (mem[cdb[i].rd_arch][PRF_IDX-1:0] == cdb[i].rd_phy) && (cdb[i].rd_arch==id_rat_itf.read_arch) ) 
+                from_id.read_phy[PRF_IDX] = '1;
+        end
+    end
 
 
 endmodule
