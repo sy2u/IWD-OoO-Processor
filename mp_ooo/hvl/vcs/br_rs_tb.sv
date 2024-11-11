07e04ab521995d0a304db878ab3b2e13e50a2d4d
kongninglai <kongning.lai@gmail.com>
2024-11-12 01:06:46 +0800

    [CP3-BR] add beq test

--- mp_ooo/hvl/vcs/br_rs_tb.sv
+++ mp_ooo/hvl/vcs/br_rs_tb.sv
@@ -16,7 +16,7 @@ module br_rs_tb;
     ds_rs_itf                   ds_br_rs_itf_i();
     cdb_itf                     cdb_itfs[CDB_WIDTH]();
     rs_prf_itf                  rs_prf_itfs[CDB_WIDTH]();
-    br_cdb_itf			br_cdb_itf();
+    br_cdb_itf			        br_cdb_itf();
     br_rs br_rs_i(
         .clk                    (clk),
         .rst                    (rst),
@@ -78,8 +78,8 @@ module br_rs_tb;
         repeat (2) @(posedge clk);
     endtask : do_reset
 
-    task test_single_instruction();
-        // single jal
+    task test_jal();
+        // jal
         // JAL x1, 0xffff (pc = 0x0000_0000)
         // x1 = 0x0000_0004, miss_predict = 1, target_address = 0x0000_ffff
         ds_br_rs_itf_i.uop.rob_id <= '0;
@@ -102,13 +102,48 @@ module br_rs_tb;
         // from_id.valid <= 1'b0
         repeat (1) @(posedge clk);
         ds_br_rs_itf_i.valid <= 1'b0;
-    endtask : test_single_instruction
 
-    task test_consecutive_no_dependency(); 
+        // cycle 3, assert cdb_itfs[2].rob_id == 0, cdb_itfs[2].rd_phy == 6'd1, cdb_itfs[2]
+        repeat (2) @(posedge clk);
+        if (cdb_itfs[2].rd_value == 32'd4 && br_cdb_itf.miss_predict == 1'b0 && br_cdb_itf.target_address == 32'h0000_ffff) begin 
+            $display("JAL test passed");
+        end else begin 
+            $display("JAL test failed");
+        end
+    endtask : test_jal
+
+    task test_beq(); 
+        // beq x0, x0, 0xffff
+        // miss_predict = 1, target_address = 0x0000_ffff
+        ds_br_rs_itf_i.uop.rob_id <= '0;
+
+        ds_br_rs_itf_i.uop.rs1_phy <= 6'd0;
+        ds_br_rs_itf_i.uop.rs1_valid <= 1'b1;
+        ds_br_rs_itf_i.uop.rs2_phy <= 6'd0;
+        ds_br_rs_itf_i.uop.rs2_valid <= 1'b1;
+        ds_br_rs_itf_i.uop.rd_phy <= 6'd1;
+        ds_br_rs_itf_i.uop.fu_opcode <= BR_BEQ;
+        ds_br_rs_itf_i.uop.pc <= 32'h0000_0000;
+        ds_br_rs_itf_i.uop.imm <= 32'h0000_ffff;
+        ds_br_rs_itf_i.uop.rd_arch <= 5'd1;
+        ds_br_rs_itf_i.uop.predict_taken <= 1'b0;
+        ds_br_rs_itf_i.uop.predict_target <= 32'h0000_0004;
+        
+        ds_br_rs_itf_i.valid <= 1'b1;
+
+        // cycle 1
+        // from_id.valid <= 1'b0
         repeat (1) @(posedge clk);
-
         ds_br_rs_itf_i.valid <= 1'b0;
-    endtask : test_consecutive_no_dependency
+
+        // cycle 3, assert cdb_itfs[2].rob_id == 0, cdb_itfs[2].rd_phy == 6'd1, cdb_itfs[2]
+        repeat (2) @(posedge clk);
+        if (br_cdb_itf.miss_predict == 1'b1 && br_cdb_itf.target_address == 32'h0000_ffff) begin 
+            $display("BEQ test passed");
+        end else begin 
+            $display("BEQ test failed");
+        end
+    endtask : test_beq
 
     task test_consecutive_with_simple_dependency(); 
         repeat (1) @(posedge clk);
@@ -140,7 +175,8 @@ module br_rs_tb;
         $fsdbDumpvars(0, "+all");
         do_reset();
 
-        test_single_instruction();
+        test_jal();
+        // test_beq();
         // test_consecutive_no_dependency();
         // test_consecutive_with_simple_dependency();
         // test_consecutive_with_simple_dependency2();
