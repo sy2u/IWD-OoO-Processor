0d160bbe49c4e34f7c18323265a966264d8d855d
kongninglai <kongning.lai@gmail.com>
2024-11-14 10:52:33 +0800

    [CP3-BR] add simple dead loop test

---
+++ mp_ooo/testcode/test_simple_br.s
@@ -0,0 +1,14 @@
+dependency_test.s:
+.align 4
+.section .text
+.globl _start
+.globl _start
+    # This program consists of small snippets
+    # containing RAW, WAW, and WAR hazards
+
+    # This test is NOT exhaustive
+_start:
+loop
+    beq x0, x0, loop
+halt:
+    slti x0, x0, -256
