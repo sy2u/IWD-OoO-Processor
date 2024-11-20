63a113072b685584b439f5cbb8280f5d334910f4
Chiming Ni <chiming2@illinois.edu>
2024-11-20 08:07:49 -0800

    Revise PRF

--- mp_ooo/hdl/backend/execute/prf.sv
+++ mp_ooo/hdl/backend/execute/prf.sv
@@ -8,12 +8,13 @@ import prf_types::*;
     cdb_itf.prf                 cdb[CDB_WIDTH]
 );
     // physical register file
-    logic [31:0]    prf_data [PRF_DEPTH];
+    logic [31:0]    prf_data            [PRF_DEPTH-1:1];
 
     // local copy of cdb interface and rs_prf_interface
     cdb_prf_t       cdb_local           [CDB_WIDTH];
     rs_prf_itf_t    from_rs_local_in    [CDB_WIDTH];
     rs_prf_itf_t    from_rs_local_out   [CDB_WIDTH];
+
     generate 
         for (genvar i = 0; i < CDB_WIDTH; i++) begin 
             assign cdb_local[i].valid       = cdb[i].valid;
@@ -39,25 +40,30 @@ import prf_types::*;
 
     always_comb begin
         for (int j = 0; j < CDB_WIDTH; j++) begin
-            from_rs_local_out[j].rs1_value = prf_data[from_rs_local_in[j].rs1_phy];
-            from_rs_local_out[j].rs2_value = prf_data[from_rs_local_in[j].rs2_phy];
-
-            for (int i = 0; i < CDB_WIDTH; i++) begin
-                if (cdb_local[i].valid && (cdb_local[i].rd_phy == from_rs_local_in[j].rs1_phy)) begin 
-                    from_rs_local_out[j].rs1_value = cdb_local[i].rd_value;
-                end
-
-                if (cdb_local[i].valid && (cdb_local[i].rd_phy == from_rs_local_in[j].rs2_phy)) begin 
-                    from_rs_local_out[j].rs2_value = cdb_local[i].rd_value;
-                end
-            end
-
             if (from_rs_local_in[j].rs1_phy == '0) begin 
                 from_rs_local_out[j].rs1_value = '0;
+            end else begin
+                from_rs_local_out[j].rs1_value = prf_data[from_rs_local_in[j].rs1_phy];
+                for (int i = 0; i < CDB_WIDTH; i++) begin
+                    if (cdb_local[i].valid && (cdb_local[i].rd_phy == from_rs_local_in[j].rs1_phy)) begin 
+                        from_rs_local_out[j].rs1_value = cdb_local[i].rd_value;
+                    end
+                end
             end
+        end
+    end
 
+    always_comb begin
+        for (int j = 0; j < CDB_WIDTH; j++) begin
             if (from_rs_local_in[j].rs2_phy == '0) begin 
                 from_rs_local_out[j].rs2_value = '0;
+            end else begin
+                from_rs_local_out[j].rs2_value = prf_data[from_rs_local_in[j].rs2_phy];
+                for (int i = 0; i < CDB_WIDTH; i++) begin
+                    if (cdb_local[i].valid && (cdb_local[i].rd_phy == from_rs_local_in[j].rs2_phy)) begin 
+                        from_rs_local_out[j].rs2_value = cdb_local[i].rd_value;
+                    end
+                end
             end
         end
     end
