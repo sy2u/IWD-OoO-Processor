bd752c2a99ff6447a05cb77fc7b0c7242b0679c8
Chiming Ni <chiming2@illinois.edu>
2024-11-26 20:25:30 +0800

    Add interfaces for Load and Store

--- mp_ooo/hdl/common/interfaces/cache_itf.sv
+++ mp_ooo/hdl/common/interfaces/cache_itf.sv
@@ -26,3 +26,49 @@ interface dmem_itf();
     );
 
 endinterface
+
+interface dmem_wo_itf();
+
+    logic   [31:0]  addr;
+    logic   [3:0]   wmask;
+    logic   [31:0]  wdata;
+    logic           resp;
+
+    modport cpu (
+        output              addr,
+        output              wmask,
+        output              wdata,
+        input               resp
+    );
+
+    modport cache (
+        input               addr,
+        input               wmask,
+        input               wdata,
+        output              resp
+    );
+
+endinterface
+
+interface dmem_ro_itf();
+
+    logic   [31:0]  addr;
+    logic   [3:0]   rmask;
+    logic   [31:0]  rdata;
+    logic           resp;
+
+    modport cpu (
+        output              addr,
+        output              rmask,
+        input               rdata,
+        input               resp
+    );
+
+    modport cache (
+        input               addr,
+        input               rmask,
+        output              rdata,
+        output              resp
+    );
+
+endinterface
